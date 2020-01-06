8dbce1b520e07e946cb7f78b5ea3daf2c6700b9b
Xing Tao Shi <xshi@stuy.edu>
2020-01-06 11:54:51 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+Write the code necessary to do the following:
+1) Select the section with an id of container without using querySelector.
+2) Select the section with an id of container using querySelector.
+3) Select all of the list items with a class of "second".
+4) Select a list item with a class of third, but only the list item inside of the ol tag.
+5) Give the section with an id of container the text "Hello!".
+6) Add the class main to the div with a class of footer.
+7) Remove the class main on the div with a class of footer.
+8) Create a new li element.
+9) Give the li the text "four".
+10) Append the li to the ul element.
+12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+13) Remove the div with a class of footer.
+ */
+
+
