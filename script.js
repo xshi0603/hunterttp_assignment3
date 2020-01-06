883292862637862dbc8ce257dd836d7679410657
Xing Tao Shi <xshi@stuy.edu>
2020-01-06 11:59:22 -0500

    more init files

--- script.js
+++ script.js
@@ -12,6 +12,32 @@ Write the code necessary to do the following:
 10) Append the li to the ul element.
 12) Loop over all of the lis inside the ol tag and give them a background color of "green".
 13) Remove the div with a class of footer.
+
+Goal:
+Demonstrate understanding of DOM traversal and DOM manipulation. Build familiarity with JS syntax. Look into: getElementById, querySelector, getElementsByClassName, querSelectorAll, the innerText property, classList.add, className, classList.remove, createElement, appendChild. Section off the code with comments based on the particular part of the prompt you are addressing. 
  */
 
+//1) Select the section with an id of container without using querySelector.
+document.getElementById("container");
 
+//2) Select the section with an id of container using querySelector.
+
+//3) Select all of the list items with a class of "second".
+
+//4) Select a list item with a class of third, but only the list item inside of the ol tag.
+
+//5) Give the section with an id of container the text "Hello!".
+
+//6) Add the class main to the div with a class of footer.
+
+//7) Remove the class main on the div with a class of footer.
+
+//8) Create a new li element.
+
+//9) Give the li the text "four".
+
+//10) Append the li to the ul element.
+
+//12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+//13) Remove the div with a class of footer.
