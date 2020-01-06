7535b7af4b196dd1f0d1c30d3114c428f36bbc37
Xing Tao Shi <xshi@stuy.edu>
2020-01-06 13:50:21 -0500

    finished challenges 1

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 /*
-Write the code necessary to do the following:
-1) Select the section with an id of container without using querySelector.
-2) Select the section with an id of container using querySelector.
-3) Select all of the list items with a class of "second".
-4) Select a list item with a class of third, but only the list item inside of the ol tag.
-5) Give the section with an id of container the text "Hello!".
-6) Add the class main to the div with a class of footer.
-7) Remove the class main on the div with a class of footer.
-8) Create a new li element.
-9) Give the li the text "four".
-10) Append the li to the ul element.
-12) Loop over all of the lis inside the ol tag and give them a background color of "green".
-13) Remove the div with a class of footer.
-
-Goal:
-Demonstrate understanding of DOM traversal and DOM manipulation. Build familiarity with JS syntax. Look into: getElementById, querySelector, getElementsByClassName, querSelectorAll, the innerText property, classList.add, className, classList.remove, createElement, appendChild. Section off the code with comments based on the particular part of the prompt you are addressing. 
+Xing Tao Shi, Camryn B.
  */
 
+
 //1) Select the section with an id of container without using querySelector.
-console.log(document.getElementById("container"));
+document.getElementById("container");
+//console.log(document.getElementById("container"));
 
 //2) Select the section with an id of container using querySelector.
-console.log(document.querySelector("#container"));
+document.querySelector("#container");
+//console.log(document.querySelector("#container"));
 
 //3) Select all of the list items with a class of "second".
-
+document.getElementsByClassName("example");
+//console.log(document.getElementsByClassName("second"));
 
 //4) Select a list item with a class of third, but only the list item inside of the ol tag.
+document.querySelector(".third", "ol");
+//console.log(document.querySelector(".third", "ol"));
 
 //5) Give the section with an id of container the text "Hello!".
+//document.getElementById("container").innerHTML = "Hello!";
 
 //6) Add the class main to the div with a class of footer.
+document.getElementsByClassName("footer")[0].classList.add("main");
 
 //7) Remove the class main on the div with a class of footer.
+document.getElementsByClassName("footer")[0].classList.remove("main");
 
 //8) Create a new li element.
+var newli = document.createElement("li"); 
 
 //9) Give the li the text "four".
+newli.innerHTML = "four";
 
 //10) Append the li to the ul element.
+document.getElementsByTagName("ul")[0].appendChild(newli);
 
 //12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+var elements = document.querySelectorAll("ol li");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = "green";
+}
 
 //13) Remove the div with a class of footer.
+var tempElement = document.getElementsByClassName("footer")[0];
+tempElement.parentNode.removeChild(tempElement);
